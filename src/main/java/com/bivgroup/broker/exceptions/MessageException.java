cb1c72d860160177d6e1f777dfb80bc4c8f88082
akustov <andreykus@mail.ru>
2016-06-17 12:07:59 +0300

    1

---
+++ src/main/java/com/bivgroup/broker/exceptions/MessageException.java
@@ -0,0 +1,34 @@
+package com.bivgroup.broker.exceptions;
+
+/**
+ * Created by bush on 12.05.2016.
+ */
+
+public class MessageException extends Exception {
+
+    private String mewebMessage = "";
+
+    public MessageException() {
+    }
+
+    public MessageException(String msg) {
+        super(msg);
+    }
+
+    public MessageException(String msg, String mewebMessage) {
+        super(msg);
+        this.mewebMessage = mewebMessage;
+    }
+
+    public MessageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MessageException(Throwable cause) {
+        super(cause);
+    }
+
+    public String getmeMessage() {
+        return mewebMessage;
+    }
+}
