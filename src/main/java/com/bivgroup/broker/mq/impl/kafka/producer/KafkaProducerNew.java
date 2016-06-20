24f3c68556a4e7ebf125276dc9ed8b23e900c480
akustov <andreykus@mail.ru>
2016-06-20 16:44:55 +0300

    add kafka

--- src/main/java/com/bivgroup/broker/mq/impl/kafka/producer/KafkaProducerNew.java
+++ src/main/java/com/bivgroup/broker/mq/impl/kafka/producer/KafkaProducerNew.java
@@ -30,24 +30,28 @@ public class KafkaProducerNew implements com.bivgroup.broker.mq.interfaces.Produ
     public Config config;
 
 
+    public KafkaProducer<Integer, String> producer;
+
+    public void init() {
+        KafkaProducer<Integer, String> producer = new KafkaProducer<Integer,
+                String>(config.getProperties());
+        this.producer = producer;
+    }
+
 
     void o() throws ExecutionException, InterruptedException {
 
 
         //1
-        KafkaProducer<Integer, String> producer1 = new KafkaProducer<Integer,
-                String>(config.getProperties());
         ProducerRecord<Integer, String> record = new ProducerRecord<Integer,
                 String>("mytesttopic", "mess");
+        this.producer.send(record);
 
+        RecordMetadata rez = this.producer.send(record).get();
 
-        producer1.send(record);
+        this.producer.send(record, new DemoProducerCallback());
 
-        RecordMetadata rez = producer1.send(record).get();
-
-        producer1.send(record, new DemoProducerCallback());
-
-        producer1.close();
+        this.producer.close();
 
         //2
         Producer<Integer, String> producer = new Producer<>(new ProducerConfig(config.getProperties()));
@@ -58,16 +62,16 @@ public class KafkaProducerNew implements com.bivgroup.broker.mq.interfaces.Produ
         //3
         Properties properties = config.getProperties();
         properties.setProperty(PARTITIONER_CLASS, "com.bivgroup.broker.mq.impl.kafka");
-        producer1 = new KafkaProducer<Integer,
+        this.producer = new KafkaProducer<Integer,
                 String>(properties);
         for (int iCount = 0; iCount < 100; iCount++) {
             int partition = iCount %
-                    producer1.partitionsFor("mytesttopic").size();
+                    this.producer.partitionsFor("mytesttopic").size();
             String message = "My Test Message No " + iCount;
             ProducerRecord<Integer, String> record1 = new
                     ProducerRecord<Integer, String>("mytesttopic",
                     partition, iCount, message);
-            producer1.send(record1);
+            this.producer.send(record1);
         }
         //4
 
