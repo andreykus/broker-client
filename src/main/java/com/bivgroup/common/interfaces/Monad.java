2917c8dff557b1f713a98ef618ec6f3974930c32
akustov <andreykus@mail.ru>
2016-07-15 15:06:17 +0300

    add kafka consumer add rabbit consumer add test

---
+++ src/main/java/com/bivgroup/common/interfaces/Monad.java
@@ -0,0 +1,77 @@
+package com.bivgroup.common.interfaces;
+
+import com.google.common.base.Function;
+
+/**
+ * Created by bush on 15.07.2016.
+ */
+public interface Monad<T> {
+
+    /**
+     * FlatMaps this value to a new value with different component type.
+     *
+     * @param mapper A mapper
+     * @param <U>    Component type of the mapped {@code AbstractMonad}
+     * @return a mapped {@code AbstractMonad}
+     * @throws NullPointerException if {@code mapper} is null
+     */
+    <U> Monad<U> flatMap(Function<? super T, ? extends Iterable<? extends U>> mapper);
+
+    /**
+     * Maps this value to a new value with different component type.
+     *
+     * @param mapper A mapper
+     * @param <U>    Component type of the mapped {@code AbstractMonad}
+     * @return a mapped {@code AbstractMonad}
+     * @throws NullPointerException if {@code mapper} is null
+     */
+    <U> Monad<U> map(Function<? super T, ? extends U> mapper);
+//
+//
+//    @Override
+//    Value<T> filter(Predicate<? super T> predicate);
+//
+//    @Override
+//    <U> Value<U> flatten();
+//
+//    @Override
+//    <U> Value<U> flatMap(Function<? super T, ? extends java.lang.Iterable<? extends U>> mapper);
+//
+//
+//
+//    @Override
+//    public Option<T> filter(Predicate<? super T> predicate) {
+//        final T value = get();
+//        if (predicate.test(value)) {
+//            return new Some<>(value);
+//        } else {
+//            return None.instance();
+//        }
+//    }
+//    @SuppressWarnings("unchecked")
+//    @Override
+//    public <U> Lazy<U> flatMap(Function<? super T, ? extends java.lang.Iterable<? extends U>> mapper) {
+//        Objects.requireNonNull(mapper, "mapper is null");
+//        if (isEmpty()) {
+//            return (Lazy<U>) this;
+//        } else {
+//            return Lazy.of(() -> Value.get(mapper.apply(get())));
+//        }
+//    }
+//
+//    @SuppressWarnings("unchecked")
+//    @Override
+//    public <U> Lazy<U> flatten() {
+//        try {
+//            return ((Lazy<? extends java.lang.Iterable<U>>) this).flatMap(Function.identity());
+//        } catch(ClassCastException x) {
+//            throw new UnsupportedOperationException("flatten of non-iterable elements");
+//        }
+//    }
+//
+//    @Override
+//    public <U> Lazy<U> map(Function<? super T, ? extends U> mapper) {
+//        return Lazy.of(() -> mapper.apply(get()));
+//    }
+}
+
---
+++ src/test/java/com/bivgroup/common/MaybeTest.java
@@ -0,0 +1,37 @@
+package com.bivgroup.common;
+
+import com.bivgroup.common.interfaces.Function;
+import com.bivgroup.common.interfaces.Maybe;
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by bush on 15.07.2016.
+ */
+public class MaybeTest extends TestCase {
+
+    public void testMap() throws Exception {
+        String s = new String("111ddssdsd23333");
+        Maybe<String> trst =
+                Maybe.instance(s).map(new Function<String, List<String>>() {
+                                          public List<String> apply(String s) {
+                                              return Arrays.asList(s.split("s"));
+                                          }
+                                      }
+                ).map(new Function<List<String>, String>() {
+                          public String apply(List<String> s) {
+                              StringBuilder sb = new StringBuilder();
+                              for (String ss : s) {
+                                  sb.append(ss);
+                              }
+                              return sb.toString();
+                          }
+                      }
+                );
+        Assert.assertEquals(trst.get(), "111dddd23333");
+
+    }
+}
